3dc03c9193c4da7664d35389087f958c5974c15c
jason <jason@10.234.161.41>
2024-01-02 19:47:53 +0800

    version10: points calculation been modified

--- test/src/main/traversal_PostOrder.java
+++ test/src/main/traversal_PostOrder.java
@@ -14,10 +14,26 @@ public class traversal_PostOrder {
 		Keyword keyword2 = new Keyword("劇情", 1.8);
 		Keyword keyword3 = new Keyword("演員", 2.1);
 		Keyword keyword4 = new Keyword("配樂", 1.2);
+		Keyword keyword5 = new Keyword("觀後", 5);
+		Keyword keyword6 = new Keyword("Audience", 1.2);
+		Keyword keyword7 = new Keyword("Critic", 1.2);
+		Keyword keyword8 = new Keyword("Review", 1.2);
+		Keyword keyword9 = new Keyword("Director", 5);
+		Keyword keyword10 = new Keyword("Rating", 1.2);
+		Keyword keyword11 = new Keyword("Cast", 5);
+		
 		keywords.add(keyword1);
 		keywords.add(keyword2);
 		keywords.add(keyword3);
 		keywords.add(keyword4);
+		keywords.add(keyword5);
+		keywords.add(keyword6);
+		keywords.add(keyword7);
+		keywords.add(keyword8);
+		keywords.add(keyword9);
+		keywords.add(keyword10);
+		keywords.add(keyword11);
+		
 		tree = T;
 	}
 
